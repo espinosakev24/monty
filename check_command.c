a9545c1627dd6adf75b0d69f788c0afc49820399
Jaime Andres Velez Rojas <jandres.vr@hotmail.com>
2019-09-25 16:33:06 -0500

    feature: function that create the structure and  another that create a double linked list

---
+++ check_command.c
@@ -0,0 +1,33 @@
+#include "monty.h"
+/**
+ * check_command - create a structure using the content  of an array
+ * @content:  double pointer
+ * Return: the head of the structure
+*/
+stack_t *check_command(char **content)
+{
+	int line = 0, i = 0, idx = 0;
+	char *val;
+	stack_t **head;
+
+	while (content[idx] != NULL)
+	{
+		val = strtok(content[idx], " ");
+		if (val == "push")
+		{
+			val = strtok(NULL, " ");
+			while (val[i] != NULL)
+			{
+				if (_isdigit(val) == 0)
+				{
+					printf("line %d: invalid input\n", line);
+					exit(1);
+				}
+				i++;
+				add_dnodeint(&head, atoi(val));
+			}
+		}
+		line++;
+	}
+	return (head);
+}

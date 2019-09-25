7e6da529a46634aeaf9e8836b57a1bf6084a20a8
Kevin <916@holbertonschool.com>
2019-09-25 14:53:33 -0500

    uploading header and chaning cpstr by strcp

--- monty.h
+++ monty.h
@@ -33,6 +33,6 @@ typedef struct instruction_s
 		char *opcode;
 		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-char *cpstr(char *dest, char *src);
 int count_line(char **argv);
+char **get_text(int argc, char **arv);
 #endif

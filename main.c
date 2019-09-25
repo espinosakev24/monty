3850d2878bfc5fbb6be6963236a1c7645003b203
Kevin <916@holbertonschool.com>
2019-09-24 20:23:43 -0500

    uploading --> main funct and header file

--- main.c
+++ main.c
@@ -1,15 +1,19 @@
 #include "monty.h"
-
 int main(int argc, char **arv)
 {
 	FILE *fp = fopen(arv[1], "r");
 	int text;
-	char *buffer[1024];
-	int fd;
-	int status = 1;
+	char **buffer = malloc(sizeof(char) * 10000);
+	int fd, a = 0, idx = 0;
+	int status = 1, value = 0;
 	size_t size = 32;
+	char **content = malloc(sizeof(char) * 10000);
 
-
+	while (value <= 15)
+	{
+		content[value] = malloc(sizeof(char) * 100);
+		value++;
+	}
 	if (argc != 2)
 	{
 		printf("USAGE: monty file\n");
@@ -24,9 +28,14 @@ int main(int argc, char **arv)
 	{
 		text = getline(buffer, &size, fp);
 		if (text == EOF)
-			free(buffer);
-			exit(1);
-		printf("%s\n", *buffer);
+			break;
+		cpstr(content[idx], *buffer);
+		idx++;
+	}
+	while (content[a] != NULL)
+	{
+		printf("%s\n", content[a]);
+		a++;
 	}
 	fclose(fp);
 	return (0);
--- monty.h
+++ monty.h
@@ -1,5 +1,10 @@
 #ifndef Monty
 #define Monty
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -28,4 +33,5 @@ typedef struct instruction_s
 		char *opcode;
 		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+char *cpstr(char *dest, char *src);
 #endif

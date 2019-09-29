a54241affcaf6d208287ae7adc2afeafb6792c43
Kevin <916@holbertonschool.com>
2019-09-28 22:26:56 -0500

    deleting line of void declaration

--- make_op.c
+++ make_op.c
@@ -25,28 +25,3 @@ int *make_op(stack_t **head, char *str, int line)
 		free_dlist(head);
 		exit(EXIT_FAILURE); }
 	return (0); }
-#include "monty.h"
-/**
- * swapping - function that swap n members of two header nodes
- * @h: double pointer to the dlinked list
- * @line: var counter of the lines of the file
- * Return: void.
- */
-void swapping(stack_t **h, int line)
-{
-	(void) line;
-	stack_t *aux = *h;
-	int num1 = 0, num2 = 0;
-
-	if (!aux->next)
-	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", line);
-		fclose(global.fp);
-		exit(EXIT_FAILURE);
-	}
-
-	num1 = aux->n;
-	num2 = aux->next->n;
-	aux->n = num2;
-	aux->next->n = num1;
-}

8859b77344e1f23cb163f6fff228cd0862c6c3bc
Kevin <916@holbertonschool.com>
2019-09-29 23:10:15 -0500

    Adding error message when empty stack

--- pchar.c
+++ pchar.c
@@ -7,6 +7,13 @@
  */
 void do_pchar(stack_t **h, int line)
 {
+	if (!*h || !h)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line);
+		fclose(global.fp);
+		exit(EXIT_FAILURE);
+		
+	}
 	if ((*h)->n > 127 || (*h)->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line);
